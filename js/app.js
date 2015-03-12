f45c4874e6903bec36df1009c2c21ea6bb31c1a7
Kevin Schneider <kevin.g.schneider@gmail.com>
2015-03-11 23:00:17 -0400

    3/11 1100pm

--- js/app.js
+++ js/app.js
@@ -6,5 +6,44 @@ $(document).ready(function() {
   .mouseleave(function() {
     $('.ryu-ready').hide();
 	$('.ryu-still').show();
-  });
+  })
+  .mousedown(function() {
+  	playHadouken();
+    $('.ryu-ready').hide();
+    $('.ryu-throwing').show();
+    $('.hadouken').finish().show().animate(
+  		{'left': '1020px'},
+  		500,
+  		function() {
+    	$(this).hide();
+    	$(this).css('left', '520px');
+  })
+  })
+  .mouseup(function() {
+    $('.ryu-throwing').hide();
+    $('.ryu-ready').show();
+    // ryu goes back to his ready position
+  })
 });
+
+$(document).keydown(function (e) {
+	if (e.keyCode == 88) {
+	$('.ryu-still').hide();
+	$('.ryu-cool').show();
+	$('.ryu-ready').hide();
+}
+});
+
+$(document).keyup(function (e) {
+	if (e.keyCode = 88) {
+	$('.ryu-still').show();
+	$('.ryu-ready').hide();
+	$('.ryu-cool').hide();
+}
+});
+
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+};
